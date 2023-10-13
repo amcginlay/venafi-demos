f51a228b0e77fe4731095595ba8f7daaba0182de
Alan McGinlay <amcginlay@gmail.com>
2023-10-13 11:43:24 +0100

    comments in purple, moved store-control-plane-api-key to own fn.

--- demos/07-tlspk-venafi-control-plane/run-demo.sh
+++ demos/07-tlspk-venafi-control-plane/run-demo.sh
@@ -18,33 +18,29 @@ function end-of-section() {
 }
 
 function delete-all-local-k8s-clusters() {
-  echo && echo "# deleting all clusters"
+  printf "\n${PURPLE}# deleting all clusters${COLOR_RESET}\n"
   pei "for cluster in \$(kind get clusters); do kind delete cluster --name \${cluster}; done"
 }
 
 export CLUSTER_NAME="k8s-$(date +'%y%m%d%H%M')"
 function create-local-k8s-cluster() {
-  echo && echo "# creating cluster"
+  printf "\n${PURPLE}# creating cluster${COLOR_RESET}\n"
   pei "kind create cluster --name ${CLUSTER_NAME}"
 }
 
 function deploy-agent() {
-  echo && echo "# registering cluster with Control Plane"  
-  # Q. why is the owner even needed when the interactive flow doesn't even ask for it?
+  printf "\n${PURPLE}# registering cluster with Control Plane${COLOR_RESET}\n"
   OWNER=InfoSec # TODO may need to parameterize the owner
-  pei "venctl installation cluster connect --name ${CLUSTER_NAME} --kubeconfig-context kind-${CLUSTER_NAME} --owning-team ${OWNER} --auto --api-key \${VCP_APIKEY}"
+  pei "venctl installation cluster connect --name ${CLUSTER_NAME} --kubeconfig-context kind-${CLUSTER_NAME} --owning-team ${OWNER} --no-prompts --api-key \${VCP_APIKEY}"
 }
 
 function deploy-components() {
-  echo && echo "# apply pull secret, build helmfile manifest and deploy required TLSPK components (~2 mins)"
+  printf "\n${PURPLE}# apply pull secret, build helmfile manifest and deploy required TLSPK components (~2 mins)${COLOR_RESET}\n"
 
   # !!! JUST EXECUTE THE FOLLOWING TO HIDE ALL THE JSCTL NASTINESS !!!
   jsctl registry auth output --format secret | yq '.metadata.name = "'"tlspk-operator"'"' | yq '.metadata.namespace = "'"venafi"'"' > ${temp_dir}/pullsecret.yaml
   REGISTRY_USERNAME=$(yq '.data."'".dockerconfigjson"'"' ${temp_dir}/pullsecret.yaml | base64 -d | jq '.auths."'"eu.gcr.io"'".username' -r)
   REGISTRY_PASSWORD=$(yq '.data."'".dockerconfigjson"'"' ${temp_dir}/pullsecret.yaml | base64 -d | jq '.auths."'"eu.gcr.io"'".password' -r)
-  # pei "cat /${temp_dir}/pullsecret.yaml"
-  # pei "echo \$REGISTRY_USERNAME"
-  # pei "echo \$REGISTRY_PASSWORD"
 
   kubectl create namespace venafi 2>/dev/null # should already exist, belt and braces
   pei "kubectl apply -f \${temp_dir}/pullsecret.yaml" # in venafi namespace
@@ -53,13 +49,13 @@ function deploy-components() {
 }
 
 function patch-approver-rbac() {
-  echo && echo "# cert-manager default approver needs RBAC coverage for VEI resource types"
+  printf "\n${PURPLE}# cert-manager default approver needs RBAC coverage for VEI resource types${COLOR_RESET}\n"
   pei "cat ven-issuer-rbac-patch.json"
   pei "kubectl -n venafi patch clusterrole cert-manager-controller-approve:cert-manager-io --type=json --patch "'"$(cat ven-issuer-rbac-patch.json)"'""
 }
 
 function create-unsafe-tls-secrets() {
-  echo && echo "# creating certificate as secret WITHOUT cert-manager (openssl)"
+  printf "\n${PURPLE}# creating certificate as secret WITHOUT cert-manager${COLOR_RESET}\n"
   pei "export common_name=unsafe.${CLUSTER_NAME}.venafidemo.com"
   pei "cat rogue-certificate-template.conf"
 
@@ -71,46 +67,45 @@ function create-unsafe-tls-secrets() {
   pei "kubectl -n demo-certs create secret tls $(tr '.' '-' <<< ${common_name})-tls --cert=\${temp_dir}/cert.pem --key=\${temp_dir}/key.pem"
 }
 
-function create-native-issuer() {
-  echo && echo "# create a secret to contain the Control Plane API key"
+function store-control-plane-api-key() {
+  printf "\n${PURPLE}# create a secret to contain the Control Plane API key${COLOR_RESET}\n"
+  kubectl create namespace venafi 2>/dev/null # should already exist, belt and braces
   pei "kubectl -n venafi create secret generic vcp-credentials --from-literal=api-key=\${VCP_APIKEY}"
+}
 
+function create-native-issuer() {
   echo && echo "# create a cluster-wide cert-manager issuer able to send CRs to the Control Plane"
   pei "cat ven-native-issuer.yaml"
   pei "kubectl apply -f ven-native-issuer.yaml"
 }
 
 function create-vei-issuer() {  
-  echo && echo "# create a secret to contain the Control Plane API key"
-  kubectl create namespace venafi 2>/dev/null # should already exist, belt and braces
-  pei "kubectl -n venafi create secret generic vcp-credentials --from-literal=api-key=\${VCP_APIKEY}"
-  
-  echo && echo "# create a re-useable Control Plane connection in the venafi namespace"
+  printf "\n${PURPLE}# create a re-useable Control Plane connection in the venafi namespace${COLOR_RESET}\n"
   pei "cat ven-connection.yaml"
   pei "kubectl -n venafi apply -f ven-connection.yaml"
   # RBAC as per https://platform.jetstack.io/documentation/configuration/venafi-connection/connection/secret (least privilege)
   
-  echo && echo "# update the RBAC to allow the venafi-connection service account to read the secret"
+  printf "\n${PURPLE}# update the RBAC to allow the venafi-connection service account to read the secret${COLOR_RESET}\n"
   pei "cat ven-connection-rbac.yaml"
   pei "kubectl -n venafi apply -f ven-connection-rbac.yaml"
 
-  echo && echo "# create a cluster-wide cert-manager issuer able to send CRs to the Control Plane"
+  printf "\n${PURPLE}# create a cluster-wide cert-manager issuer able to send CRs to the Control Plane${COLOR_RESET}\n"
   pei "cat ven-issuer.yaml"
   pei "kubectl apply -f ven-issuer.yaml"
 }
 
 function create-safe-tls-secrets() {
-  echo && echo "# creating certificates as secrets WITH cert-manager"
+  printf "\n${PURPLE}# creating certificates as secrets WITH cert-manager${COLOR_RESET}\n"
   pei "kubectl create namespace demo-certs 2>/dev/null"
   pei "cat ven-certificate-template.yaml"
 
-  echo && echo "# create VALID cert (venafidemo.com)"
+  printf "\n${PURPLE}# create VALID cert (venafidemo.com)${COLOR_RESET}\n"
   pei "export common_name=valid.${CLUSTER_NAME}.venafidemo.com"
   pei "export secret_name=valid-${CLUSTER_NAME}-venafidemo-com-tls"
   pei "export duration_hrs=1800" # 75 days
   pei "cat ven-certificate-template.yaml | envsubst | kubectl -n demo-certs apply -f -"
 
-  echo && echo "# create INVALID cert (venafitest.com)"
+  printf "\n${PURPLE}# create INVALID cert (venafitest.com)${COLOR_RESET}\n"
   pei "export common_name=invalid.${CLUSTER_NAME}.venafitest.com"
   pei "export secret_name=invalid-${CLUSTER_NAME}-venafitest-com-tls"
   pei "export duration_hrs=1140" # 60 days
@@ -151,6 +146,9 @@ patch-approver-rbac
 end-of-section
 
 create-unsafe-tls-secrets
+end-of-section
+
+store-control-plane-api-key
 end-of-section
 
 # *** using VEI in preference to native - also possible bug where native issuer ignores duration ***
